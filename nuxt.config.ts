b2febd8c774828579947babc0a94a54e33e0397e
Cyrus Besabella <cabesabella@joyride.com.ph>
2025-05-11 16:52:06 +0800

    add working tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,18 @@
 import Components from "unplugin-vue-components/vite";
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ["@/assets/css/main.css"],
+  css: ["~/assets/css/main.css"],
   modules: ["nuxt-auth-utils", "@nuxt/ui"],
   vite: {
     plugins: [
       Components({
         resolvers: [AntDesignVueResolver({ resolveIcons: true })],
       }),
+      tailwindcss(),
     ],
   },
 });
